c4d08e1453a5f4822ac84e7a219ae2d5c76529ea
Vocabulary-splat <jackthedoctor05@gmail.com>
2024-12-12 09:58:13 -0500

    UPDATED CLASS

--- Apples/connectfirebase.js
+++ Apples/connectfirebase.js
@@ -1,36 +1,42 @@
 import { app } from "./firebase.js";
-import{getFirestore, collection, getDocs, addDoc,} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"; 
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-const booksListRef = document.querySelector("#booksList")
+const booksListRef = document.querySelector("#booksList");
 
-const db = getFirestore(app); 
+const db = getFirestore(app);
 
 async function getBooks() {
-    const booksCollection = collection(db, "books");
-    const bookDocs = await getDocs(booksCollection);
+  const booksCollection = collection(db, "books");
+  const bookDocs = await getDocs(booksCollection);
 
-
-booksListRef.innerHTML = "";
-for (let i = 0; i < bookDocs.docs.length; i++) {
+  booksListRef.innerHTML = "";
+  for (let i = 0; i < bookDocs.docs.length; i++) {
     const books = bookDocs.docs[i];
     const bookData = books.data();
 
     booksListRef.innerHTML += `<li>${bookData.title} (${bookData.yearPublished})</li>`;
-
-}
+  }
 }
 
 async function addBook() {
-    const bookName = prompt("Toppings:");
-    const yearPublished = prompt("Quantity:");
+  const bookName = prompt("Toppings:");
+  const yearPublished = prompt("Quantity:");
 
-    const booksCollection = collection(db, "books");
+  const booksCollection = collection(db, "books");
 
-    await addDoc(booksCollection, { title: bookName, yearPublished: yearPublished });
+  await addDoc(booksCollection, {
+    title: bookName,
+    yearPublished: yearPublished,
+  });
 
-    getBooks();
+  getBooks();
 }
 
-document.querySelector("#addFoodButton").onclick = addBook;
+document.querySelector(".addFoodButton").onclick = addBook;
 
 getBooks();
