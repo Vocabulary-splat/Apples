ce61a5e794e44037fb59d12317c5b99d992ba049
cruzmas <cruzmas@iu.edu>
2024-12-11 21:43:07 -0500

    Create connectfirebase.js

---
+++ Apples/connectfirebase.js
@@ -0,0 +1,36 @@
+import { app } from "./firebase.js";
+import{getFirestore, collection, getDocs, addDoc,} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"; 
+
+const booksListRef = document.querySelector("#booksList")
+
+const db = getFirestore(app); 
+
+async function getBooks() {
+    const booksCollection = collection(db, "books");
+    const bookDocs = await getDocs(booksCollection);
+
+
+booksListRef.innerHTML = "";
+for (let i = 0; i < bookDocs.docs.length; i++) {
+    const books = bookDocs.docs[i];
+    const bookData = books.data();
+
+    booksListRef.innerHTML += `<li>${bookData.title} (${bookData.yearPublished})</li>`;
+
+}
+}
+
+async function addBook() {
+    const bookName = prompt("Book Name:");
+    const yearPublished = prompt("Year Published: ");
+
+    const booksCollection = collection(db, "books");
+
+    await addDoc(booksCollection, { title: bookName, yearPublished: yearPublished });
+
+    getBooks();
+}
+
+document.querySelector("#addFoodButton").onclick = addBook;
+
+getBooks();
